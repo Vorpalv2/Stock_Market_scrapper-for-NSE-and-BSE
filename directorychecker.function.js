c079e279d17ff310854bfaac22db5002bf323fc1
Aakash <aakashanand1995@gmail.com>
2024-02-17 15:27:22 +0530

    working on directorychecker -2

--- directorychecker.function.js
+++ directorychecker.function.js
@@ -13,18 +13,21 @@ async function createDirectory(DirectoryName) {
 }
 
 async function checkDirectory() {
+  //possibly implement a check here to see if the storage directory already exists or not?
   try {
     let createdDirectory = await createDirectory("storage", rootDir);
     console.log(createdDirectory);
     let openedDirectory = await fsm.opendir(createdDirectory, {
       recursive: true,
     });
+    // here as well to see if the directories images and PDF inside storage directory exists or not
     console.log(openedDirectory);
     await createDirectory(
       path.join(openedDirectory.path, "images"),
       storagePath
     );
     await createDirectory(path.join(openedDirectory.path, "PDF"), storagePath);
+    ////////////////
   } catch (error) {
     throw new Error(
       "Error Checking Directory : " + error + " with code " + error.errno
