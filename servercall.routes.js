b071653683d20d202cd85977c7376fbb45991260
Aakash <aakashanand1995@gmail.com>
2024-02-19 18:30:30 +0530

    allowing all

--- servercall.routes.js
+++ servercall.routes.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://stock-market-scrapper-for-nse-and-bse.vercel.app/",
+    origin: "*",
     // origin: "http://localhost:5173",
   })
 );
