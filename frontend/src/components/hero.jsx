cf04d9a25d7a97948ddbcd0267a69a42a411f991
Aakash <aakashanand1995@gmail.com>
2024-08-06 16:25:30 +0530

    more updates

--- frontend/src/components/hero.jsx
+++ frontend/src/components/hero.jsx
@@ -6,7 +6,7 @@ function CompanyForm() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await fetch("http://localhost:3000/form-submit", {
+    await fetch(import.meta.env.VITE_FETCHURL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
